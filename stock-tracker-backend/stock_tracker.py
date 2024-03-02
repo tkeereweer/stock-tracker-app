98ccffacacadf7c10f71633760ea64cb842c00d9
thijskeereweer <tkeereweer0402@gmail.com>
2024-03-02 16:22:33 +0100

    implement total portfolio value in backend

--- stock-tracker-backend/stock_tracker.py
+++ stock-tracker-backend/stock_tracker.py
@@ -13,19 +13,7 @@ CORS(app)
 # API_KEY = os.getenv('API_KEY')
 API_KEY = 'UDJ8FEEUYB4NYVJ6'
 
-# get the portfolio of a user
-@app.route('/<user_id>')
-def index(user_id):
-    portfolio = get_portfolio(user_id)
-    return jsonify(portfolio)
-
-# get the stock info of a symbol from the Alpha Vantage API
-@app.route('/stockinfo/<symbol>')
-def stock_info(symbol):
-    response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={API_KEY}") 
-    data = response.json()
-    output = list(data['Time Series (Daily)'].items())[0:5]
-    return jsonify(output)
+stock_values = {}
 
 # get stocks in a user's portfolio, hardcoded for now
 def get_portfolio(user_id):
@@ -42,6 +30,41 @@ def get_portfolio(user_id):
         }
     }
     return user_portfolio_db.get(user_id, {})
+
+# get values for stocks
+def get_values(portfolio):
+    for stock in portfolio:
+        response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={API_KEY}") 
+        data = response.json()
+        stock_values_lst = list(data['Time Series (Daily)'].items())[0:5]
+        stock_values[stock] = stock_values_lst
+    return stock_values
+
+# get the portfolio of a user
+@app.route('/<user_id>')
+def index(user_id):
+    portfolio = get_portfolio(user_id)
+    get_values(portfolio.keys())
+    output = []
+    total_value = 0
+    for stock in portfolio:
+        close_price = float(stock_values[stock][0][1]['4. close'])
+        stock_value = round(close_price * portfolio[stock], 2)
+        total_value += stock_value
+        output.append({stock: {
+            'quantity': portfolio[stock],
+            'value': stock_value
+            }})
+    output.insert(0, {'total_value': total_value})
+    return jsonify(output)
+
+# serve the stock info for a given symbol
+@app.route('/stockinfo/<symbol>')
+def stock_info(symbol):
+    if symbol not in stock_values:
+        get_values([symbol])
+    output = stock_values[symbol]
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run()
